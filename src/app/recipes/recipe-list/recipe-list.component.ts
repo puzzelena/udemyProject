e0ae044d8805dda39bd13855675c5b0f6567b46e
puzzelena <puzic.elena1@gmail.com>
2021-08-15 15:49:30 +0300

    outputting a list of recipes using ngFor directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
+    new Recipe('Potato', 'Sweet potato', 'https://cdn.pixabay.com/photo/2016/11/06/23/28/yam-1804451_960_720.jpg'),
     new Recipe('Potato', 'Sweet potato', 'https://cdn.pixabay.com/photo/2016/11/06/23/28/yam-1804451_960_720.jpg') // here we call a constructor defined in the Recipe model
   ]; // here we define the model for recipes defined in the Recidpe model file class wih constructor
   // Recipe[] is an array of recipe, now every elem passed in the array will be a part of the recipe object
